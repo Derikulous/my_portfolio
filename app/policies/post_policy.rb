a8d5c4ac6d82757397522da6ee87967feea57d73
Derik Strattan <derik.strattan@gmail.com>
2013-10-22 07:47:25 -0700

    Fix post policy to include new structure for authors and editors

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -26,15 +26,16 @@
       end
     end
 
-    class Scope < Struct.new(:user, :scope)
-      def resolve
-        if user.present? && user.editor?
-          scope.all
-        elsif user.present? && user.author?
-          scope.where(:author_id => user.id) | scope.published
-        else
-          scope.where(:published => true)
+    PostPolicy = Struct.new(:user, :post) do
+      Scope = Struct.new(:user, :scope) do
+        def resolve
+          if user.present? && user.editor?
+            scope.all
+          elsif user.present? && user.author?
+            scope.where(:author_id => user.id) | scope.published
+          else
+            scope.where(:published => true)
+          end
         end
       end
     end
-  end
