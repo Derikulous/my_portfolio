27fb6f115f5b9be188cfdfe7ca145c063dfcd075
Derik Strattan <derik.strattan@gmail.com>
2013-10-23 17:37:46 -0700

    Finish adding approval of comments on posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   include Rakismet::Model
   belongs_to :commentable, polymorphic: true
   attr_accessible :commentable, :commentable_id, :comment, :approved, :author, :author_email, :author_url, :content, :referrer, :user_agent, :user_ip, :post_id
+
+  def authored_by?(user)
+    author == user
+  end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,39 +1,38 @@
-   class PostPolicy < ApplicationPolicy
-    attr_reader :user, :post
+class PostPolicy < ApplicationPolicy
+  attr_reader :user, :post
 
-    def initialize(user, post)
-      @user = user
-      @post = post
-    end
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
 
-    def create?
-      user.author? || user.editor? if user.present?
-    end
+  def create?
+    user.author? || user.editor? if user.present?
+  end
 
-    def publish?
-      user.editor? if user.present?
-    end
+  def publish?
+    user.editor? if user.present?
+  end
 
-    def update?
-      post.authored_by?(user) || user.editor? if user.present?
-    end
-    alias_method :destroy?, :update?
+  def update?
+    post.authored_by?(user) || user.editor? if user.present?
+  end
+  alias_method :destroy?, :update?
 
-    def destroy?
-      post.authored_by?(user) || user.editor? if user.present?
-    end
+  def destroy?
+    post.authored_by?(user) || user.editor? if user.present?
+  end
 
-    PostPolicy = Struct.new(:user, :post) do
-      Scope = Struct.new(:user, :scope) do
-        def resolve
-          if user.present? && user.editor?
-            scope.all
-          elsif user.present? && user.author?
-            scope.where(:author_id => user.id) | scope.published
-          else
-            scope.where(:published => true)
-          end
-        end
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      if user.present? && user.editor?
+        scope.all
+      elsif user.present? && user.author?
+        scope.where(:author_id => user.id) | scope.published
+      else
+        scope.where(:published => true)
       end
     end
   end
+end
+
