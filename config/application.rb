c6aa9bcea180d8b81d5e918275def9df95b0428d
Derik Strattan <derik.strattan@gmail.com>
2013-11-16 11:16:29 -0800

    Added CORS gem for header response

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,12 @@ module Portfolio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.rakismet.url = 'http://www.derikstrattan.com/'
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :options]
+      end
+    end
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
