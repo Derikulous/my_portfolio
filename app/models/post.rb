6f67d53de9afb29be39a34558d843af0dc6bfe39
Derik Strattan <derik.strattan@gmail.com>
2013-11-19 13:47:04 -0800

    add image migration and model goodness

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :published, :post_id
+  attr_accessible :content, :title, :published, :post_id, :image, :remote_image_url
   belongs_to :author, class_name: "User"
   scope :published, where(published: true)
   has_many :comments, as: :commentable
+  mount_uploader :image, ImageUploader
 
   def publish!
     self.published = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Portfolio::Application.routes.draw do
+
   resources :posts
   root to: 'home#index'
 
