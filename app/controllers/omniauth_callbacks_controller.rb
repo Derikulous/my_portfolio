d263e3b2a86648b7bb0bb9b7f9475b60b927def4
Derik Strattan <derik.strattan@gmail.com>
2014-01-09 20:07:05 -0800

    Deleted omniauth. Changed width of global container

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class OmniauthCallbacksController < ApplicationController
-  def all
-    user = User.from_omniauth(request.env["omniauth.auth"])
-    if user.persisted?
-      flash.notice = "#{user.name}, you are signed in!"
-      sign_in_and_redirect user
-    else
-      session["devise.user_attributes"] = user.attributes
-      redirect_to new_user_registration_path
-    end
-  end
-  alias_method :twitter, :all
+  # def all
+  #   user = User.from_omniauth(request.env["omniauth.auth"])
+  #   if user.persisted?
+  #     flash.notice = "#{user.name}, you are signed in!"
+  #     sign_in_and_redirect user
+  #   else
+  #     session["devise.user_attributes"] = user.attributes
+  #     redirect_to new_user_registration_path
+  #   end
+  # end
+  # alias_method :twitter, :all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: "author_id"
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable
+  # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role
