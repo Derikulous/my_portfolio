ed911aecca51bf1c1cfd9465bba5bbb3114b8ca3
Derik Strattan <derik.strattan@gmail.com>
2014-01-10 18:12:43 -0800

    projects pages fully built out

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Portfolio::Application.routes.draw do
   get '/about', to: 'pages#about'
   get '/about/2', to: 'pages#bio'
   get '/projects', to: 'pages#projects'
-  get '/projects/2', to: 'pages#projects_2'
-  get '/projects/3', to: 'pages#projects_3'
-  get '/projects/4', to: 'pages#projects_4'
+  get '/projects/officehours', to: 'pages#projects_2'
+  get '/projects/yolo', to: 'pages#projects_3'
+  get '/projects/portfolio', to: 'pages#projects_4'
 
   root to: 'home#index'
 
