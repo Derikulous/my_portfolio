8c5519e1b58eb89753b2fb78bdd993b9b261c10a
Derik Strattan <derik.strattan@gmail.com>
2014-03-27 00:35:55 -0700

    add startup weekend redesign seattle project. upgrade Ruby to 2.1

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Portfolio::Application.routes.draw do
   get '/projects/officehours', to: 'pages#projects_2'
   get '/projects/yolo', to: 'pages#projects_3'
   get '/projects/portfolio', to: 'pages#projects_4'
+  get '/projects/stake', to: 'pages#projects_5'
   get '/photos', to: 'pages#photos'
   get '/photos/2', to: 'pages#photos_2'
 
   root to: 'home#index'
 
 end
-
