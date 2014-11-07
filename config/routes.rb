4154730d4b78d8066e362d34967c90a1a2d57c3d
Derik Strattan <derik.strattan@gmail.com>
2014-11-06 20:42:10 -0800

    add photos design

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Portfolio::Application.routes.draw do
   # Custom routes for static pages
   get '/logout', to: 'pages#signout'
   get '/about', to: 'pages#about'
-  get '/about/2', to: 'pages#bio'
-  get '/projects', to: 'pages#projects'
-  get '/projects/officehours', to: 'pages#projects_2'
-  get '/projects/yolo', to: 'pages#projects_3'
-  get '/projects/portfolio', to: 'pages#projects_4'
-  get '/projects/stake', to: 'pages#projects_5'
+  # get '/about/2', to: 'pages#bio'
+  # get '/projects', to: 'pages#projects'
+  # get '/projects/officehours', to: 'pages#projects_2'
+  # get '/projects/yolo', to: 'pages#projects_3'
+  # get '/projects/portfolio', to: 'pages#projects_4'
+  # get '/projects/stake', to: 'pages#projects_5'
   get '/photos', to: 'pages#photos'
-  get '/photos/2', to: 'pages#photos_2'
+  # get '/photos/2', to: 'pages#photos_2'
 
   root to: 'home#index'
 
